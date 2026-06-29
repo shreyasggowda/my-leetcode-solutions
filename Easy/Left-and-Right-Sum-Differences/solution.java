6dc5b75776d045eb900e636456799c99a3c4c506
Shreyas G Gowda <shreyuggowda1@gmail.com>
2026-06-29 13:19:59 +0530

    Solved: Left and Right Sum Differences (Easy) Language: Java Date: 2026-06-29T07:49:58.435Z

--- Easy/Left-and-Right-Sum-Differences/solution.java
+++ Easy/Left-and-Right-Sum-Differences/solution.java
@@ -1,18 +1,18 @@
-class Solution {
-    public int[] leftRightDifference(int[] nums) {
-        int n = nums.length;
-        int ts = 0;
-
-        for (int num : nums) {
-            ts += num;
-        }
-        int ans[] = new int[n];
-        int lS = 0;
-        for (int i = 0; i < n; i++) {
-            int rS = ts - lS - nums[i];
-            ans[i] = Math.abs(lS - rS);
-            lS += nums[i];
-        }
-        return ans;
-    }
-}
+1class Solution {
+2    public int[] leftRightDifference(int[] nums) {
+3        int n = nums.length;
+4        int ts = 0;
+5
+6        for (int num : nums) {
+7            ts += num;
+8        }
+9        int ans[] = new int[n];
+10        int lS = 0;
+11        for (int i = 0; i < n; i++) {
+12            int rS = ts - lS - nums[i];
+13            ans[i] = Math.abs(lS - rS);
+14            lS += nums[i];
+15        }
+16        return ans;
+17    }
+18}
